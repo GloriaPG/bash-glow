2d233d629d85ae4c4063cd1439fa2c03142b3ace
GloriaPG <ing.gloriapalmagonzalez@gmail.com>
2015-05-24 01:36:44 -0500

    Add switch bash

--- deploy.sh
+++ deploy.sh
@@ -1,27 +1,128 @@
 #!/bin/bash
 #author: Gloria Palma Gonzalez ing.gloriapalmagonzalez@gmail.com
-#desc: Script for deploy callcenter or backoffice .
+#desc: Script for deploy callcenter or backoffice.
+#Instrunctions:
+# For execute this script you should pass  parameter
+# 1.- The application for deploy, bc it's for deploy in backoffice and callcenter, c it's for deploy only callcenter and b it's for deploy only backoffice
+# example : ./deploy.sh bc
 echo "Starting deploy ... glow glow"
+#init vars
+FILE="$(date +"%Y-%m-%d")-dump.sql.gz"
+BACKUPSNAME="$(date +"%Y-%m-%d")"
 #Stop service apache for make the backups database
+echo "Go to stop service tomcat"
 service tomcat stop
 #Make de bakups database
-mysqldump --user=ventakmp --password=ws4.=Rt5gSd34 ventakmpscc >  ./dumps/dump-${1}.sql
+echo "Go to make dumps"
+mysqldump --user=ventakmp --password=ws4.=Rt5gSd34 ventakmpscc >  ./dumps/"$FILE"
+chmod 776 ./dumps/"$FILE"
 #Bakup apps
 cd /usr/local/tomcat7/webapps/
-mv  backoffice.war backoffice-${1}
-mv  callcenter.war callcenter-${1}
-# Deleted folders app
-rm -rf callcenter
-rm -rf backoffice
-#Return at root directory
-cd
-#Copy the wars in webapps
-cp ./callcenter.war  /usr/local/tomcat7/webapps
-cp ./backoffice.war  /usr/local/tomcat7/webapps
-#Start service tomcat
-service tomcat start
-echo "Finished deploy ... i know, i'm very cool"
-#Let me see de log babe
-tail -f /usr/local/tomcat7/logs/catalina.out
+case "$1" in
+    "bc")
+		echo 'Creating backups callcenter and backoffice'
+        #Backups apps
+		mv  callcenter.war callcenter-"$BACKUPSNAME"
+        mv  backoffice.war backoffice-"$BACKUPSNAME"
+        ls
+        # Deleted folders app
+        echo 'Deleting folders callcenter and backoffice'
+		rm -rf callcenter
+		rm -rf backoffice
+		ls
+		#Return at root directory
+		echo 'Returning to directory root'
+		cd
+		ls
+		#Set permits to the wars
+		echo "Setting permits tho the wars"
+		chmod -R 777 callcenter.war
+		chmod -R 777 backoffice.war
+		ls -la
+		#Copy the wars in webapps
+		echo "Copiying wars in /usr/local/tomcat7/webapps"
+		cp ./callcenter.war  /usr/local/tomcat7/webapps
+		cp ./backoffice.war  /usr/local/tomcat7/webapps
+		cd /usr/local/tomcat7/webapps
+		ls
+		cd
+		#Start service tomcat
+		echo "Go to start service tomcat"
+		service tomcat start
+		echo "Finished deploy backoffice and callcenter ;)... i know, i'm very cool"
+		#Let me see de log babe
+		echo "Hey! You should see the logs"
+		tail -f /usr/local/tomcat7/logs/catalina.out
+        ;;
+    "b")
+		echo 'Creating backup backoffice'
+        #Backups apps
+        mv  backoffice.war backoffice-"$BACKUPSNAME"
+        ls
+        # Deleted folders app
+        echo 'Deleting folder backoffice'
+		rm -rf backoffice
+		ls
+		#Return at root directory
+		echo 'Returning to directory root'
+		cd
+		ls
+		#Set permits to the wars
+		echo "Setting permits tho the wars"
+		chmod -R 777 backoffice.war
+		ls -la
+		#Copy the wars in webapps
+		echo "Copiying war in /usr/local/tomcat7/webapps"
+		cp ./backoffice.war  /usr/local/tomcat7/webapps
+		cd /usr/local/tomcat7/webapps
+		ls
+		cd
+		#Start service tomcat
+		echo "Go to start service tomcat"
+		service tomcat start
+		echo "Finished deploy backoffice and callcenter ;)... i know, i'm very cool"
+		#Let me see de log babe
+		echo "Hey! You should see the logs"
+		tail -f /usr/local/tomcat7/logs/catalina.out
+    "c")
+        echo 'Creating backup callcenter'
+        #Backups apps
+		mv  callcenter.war callcenter-"$BACKUPSNAME"
+        ls
+        # Deleted folders app
+        echo 'Deleting folder callcenter'
+		rm -rf callcenter
+		ls
+		#Return at root directory
+		echo 'Returning to directory root'
+		cd
+		ls
+		#Set permits to the wars
+		echo "Setting permits tho the war"
+		chmod -R 777 callcenter.war
+		ls -la
+		#Copy the wars in webapps
+		echo "Copiying wars in /usr/local/tomcat7/webapps"
+		cp ./callcenter.war  /usr/local/tomcat7/webapps
+		cp ./backoffice.war  /usr/local/tomcat7/webapps
+		cd /usr/local/tomcat7/webapps
+		ls
+		cd
+		#Start service tomcat
+		echo "Go to start service tomcat"
+		service tomcat start
+		echo "Finished deploy callcenter ;)... i know, i'm very cool"
+		#Let me see de log babe
+		echo "Hey! You should see the logs"
+		tail -f /usr/local/tomcat7/logs/catalina.out
+        ;;
+    *)
+        echo "You should introduce the parameter for deploy application:"
+        echo "bc : is for deploy in callcenter and backoffice"
+        echo "c : is for deploy in callcenter"
+        echo "b : is for deploy in callcenter"
+        echo "Example : ./deploy.sh bc"
+        ;; 
+esac
 
 echo #
